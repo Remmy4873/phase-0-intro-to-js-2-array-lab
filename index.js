304a178cb5825b2e64669b9d892f09cff356bb3b
Remmy4873 <Remmykiplangat4873@gmail.com>
2024-07-02 11:54:47 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+
+// index.js
+
+// Initial cats array
+let cats = ["Milo", "Otis", "Garfield"];
+
+// Function to destructively append a cat
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+// Function to destructively prepend a cat
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+// Function to destructively remove the last cat
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+// Function to destructively remove the first cat
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// Function to append a cat and return a new array
+function appendCat(name) {
+  return [...cats, name];
+}
+
+// Function to prepend a cat and return a new array
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+// Function to remove the last cat and return a new array
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1);
+}
+
+// Function to remove the first cat and return a new array
+function removeFirstCat() {
+  return cats.slice(1);
+}
